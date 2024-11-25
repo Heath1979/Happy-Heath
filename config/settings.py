1fcf9ee7e586a6fdd24b1c1120b875aa17a05a30
Heath Croasdale <thebeast126@hotmail.com>
2024-11-25 11:48:27 +0000

    set up Procfile

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-heath1979-happyheath-tgldr0pmh7b.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-heath1979-happyheath-tgldr0pmh7b.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 'https://*.herokuapp.com']
 
