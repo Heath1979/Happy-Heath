18b926ee990a2c45779fddbf7879a2d59c808b80
Heath Croasdale <thebeast126@hotmail.com>
2024-11-26 12:44:14 +0000

    add Comment model to blog models.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post, Category, Location
+from .models import Post, Category, Location, Comment
 
 # Register your models here.
 admin.site.register(Post)
@@ -8,3 +8,5 @@ admin.site.register(Post)
 admin.site.register(Category)
 
 admin.site.register(Location)
+
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -58,3 +58,22 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} | Written by {self.author}"    
+
+
+class Comment(models.Model):
+    """
+    stores a single comment entry related to :model: 'auth.User'
+    and :model: 'blog.Post'.
+    """
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+            return f"Comment {self.body} by {self.author}"
