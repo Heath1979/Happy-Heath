5fe552c2610e9ca3082c5cbbdb90a0f5d5065152
Heath Croasdale <thebeast126@hotmail.com>
2024-12-04 09:11:46 +0000

    amend location field in PostForm to CharField

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,7 @@ from .models import Comment, Post
 
 
 class PostForm(forms.ModelForm):
+    location = forms.CharField(max_length=30)
     """ 
     Post class for users to create a post
     """
@@ -25,16 +26,16 @@ class PostForm(forms.ModelForm):
         ]
 
         labels = {
-            "title": "Business name",
-            "featured_image": "Featured image",
-            "location": "Location",
-            "category": "Category",
-            "rating": "Happy Heath or Grumpy Croasdale",
-            "content": "Write your Blog",
-            "excerpt": "Your experience in one sentence",
+            "title": "Business name.",
+            "featured_image": "Featured image.",
+            "location": "Location.",
+            "category": "Category.",
+            "rating": "Happy Heath or Grumpy Croasdale?",
+            "content": "Write your Blog.",
+            "excerpt": "Your experience in one sentence.",
             "social_links": "Facebook page",
             "web_links": "Business Website",
-            "video_links": "supporting YouTube channel or video",
+            "video_links": "Supporting YouTube channel or video.",
         }
 
 class CommentForm(forms.ModelForm):
